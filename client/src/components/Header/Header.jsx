7e50540985d46bedd90d34bd2663004f13bd68ea
sweedalgharshi <sweedalgracius@gmail.com>
2023-07-18 12:43:54 +0530

    tailwind css reusable classes

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,4 +1,15 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="header flex items-center">
+      <div className="container">
+        <div className="flex items-center justify-between">
+          {/* Logo */}
+          <div>
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
 export default Header;
