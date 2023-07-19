333c9f929f3171b8de932339cebcea933ef4d2f4
sweedalgharshi <sweedalgracius@gmail.com>
2023-07-19 12:36:08 +0530

    about section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -7,6 +7,7 @@ import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
 
 import { BsArrowRight } from "react-icons/bs";
+import About from "../components/About/About";
 
 function Home() {
   return (
@@ -152,6 +153,10 @@ function Home() {
           </div>
         </div>
       </section>
+
+      {/* ========= About section start ==========*/}
+      <About />
+      {/* ========= About section end ============*/}
     </>
   );
 }
